c982e36de81e6beb4d84c2ff2d7173a9e9b5458f
SamaraaTorres <samaratorres050@hotmail.com>
2023-02-09 16:40:34 -0300

    [Group 7] Requiriment 1

---
+++ src/pages/Login.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+  state = {
+    player: '',
+    email: '',
+    disable: true,
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    }, () => {
+      const { player, email } = this.state;
+      if (player !== '' && email !== '') {
+        this.setState({
+          disable: false,
+        });
+      } else {
+        this.setState({
+          disable: true,
+        });
+      }
+    });
+  }
+
+  render() {
+    const { disable, player, email } = this.state;
+    return (
+      <div>
+        <input
+          type="text"
+          data-testid="input-player-name"
+          value={ player }
+          name="player"
+          onChange={ this.handleChange }
+        />
+        <input
+          type="email"
+          data-testid="input-gravatar-email"
+          value={ email }
+          name="email"
+          onChange={ this.handleChange }
+        />
+        <button type="button" data-testid="btn-play" disabled={ disable }>Play</button>
+      </div>
+    );
+  }
+}
