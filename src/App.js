22f4166ccbd1968c65bf6b04c9bb80b64f32e9be
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-02-10 16:38:48 -0300

    Requeriment - 12 e 13

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Login from './pages/Login';
 import Game from './pages/Game';
 import Ranking from './pages/Ranking';
 import Settings from './pages/Settings';
+import Feedback from './pages/Feedback';
 import './App.css';
 
 export default function App() {
   return (
     <Switch>
+      <Route path="/feedback" component={ Feedback } />
       <Route path="/ranking" component={ Ranking } />
       <Route path="/game" component={ Game } />
       <Route path="/settings" component={ Settings } />
---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,84 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import md5 from 'crypto-js/md5';
+import { connect } from 'react-redux';
+import teste from '../assets/teste.png';
+
+class Feedback extends Component {
+  state = {
+    result: 4,
+    number: 1,
+  };
+
+  render() {
+    const { result, number } = this.state;
+    const { player, email, history } = this.props;
+    return (
+      <div>
+        <section>
+          <img src={ teste } alt="teste" />
+        </section>
+        <section>
+          <figure>
+            <img
+              src={ `https://www.gravatar.com/avatar/${md5(email).toString()}` }
+              alt=""
+              data-testid="header-profile-picture"
+            />
+            <h1
+              data-testid="header-player-name"
+            >
+              { player }
+            </h1>
+          </figure>
+        </section>
+        <section>
+          <p data-testid="header-score">{result}</p>
+        </section>
+        <section>
+          {
+            result < number
+              ? <p data-testid="feedback-text">Could be better...</p>
+              : <p data-testid="feedback-text">Well Done!</p>
+          }
+        </section>
+        <section>
+          <p>{`Você acertou ${result} questões!`}</p>
+          <p>{`Um total de ${number} pontos`}</p>
+        </section>
+        <div>
+          <button
+            onClick={ () => {
+              history.push('/ranking');
+            } }
+          >
+            VER RANKING
+          </button>
+          <button
+            onClick={ () => {
+              history.push('/game');
+            } }
+          >
+            JOGAR NOVAMENTE
+
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  player: state.user.player,
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Feedback);
+
+Feedback.propTypes = {
+  player: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
